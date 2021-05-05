33f4ed3ef6418187ef42058d4718b38980b23307
BOCKELADNT Amelie <ameliebock@hotmail.fr>
2021-05-05 15:29:24 +0200

    Update backend data file and .gitignore

--- backend/data.js
+++ backend/data.js
@@ -18,10 +18,10 @@ const data = {
     ],
     products:[
         {
-            _id:1,            
             name:"ARTEZA Outils de Sculpture",
             image:"/images/01.jpg",
             category:"equipment stencils",
+            description:"blabla",
             price:30.99,
             countInStock:2,
             numReviews:8,
@@ -29,10 +29,10 @@ const data = {
             website:"https://www.amazon.fr/ARTEZA-Ensemble-Sculpture-Professionnel-Cartonn%C3%A9e/dp/B074SXW7X3/ref=asc_df_B074SXW7X3/?tag=googshopfr-21&linkCode=df0&hvadid=227964555245&hvpos=&hvnetw=g&hvrand=7371772830080740014&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9055818&hvtargid=pla-420717610049&psc=1"
         },
         {
-            _id:2,            
             name:"SENHAI Outils de sculpture pour pâte polymère",
             image:"/images/02.jpg",
-            category:"equipment stencils",            
+            category:"equipment stencils",
+            description:"blabla",
             price:12.99,
             countInStock:8,
             numReviews:8,
@@ -40,10 +40,10 @@ const data = {
             website:"https://www.amazon.fr/sculpture-polym%C3%A8re-caoutchouc-dempreinte-c%C3%A9ramique/dp/B07S8TH4KV/ref=asc_df_B07S8TH4KV/?tag=googshopfr-21&linkCode=df0&hvadid=354074254997&hvpos=&hvnetw=g&hvrand=7371772830080740014&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9055818&hvtargid=pla-777534104593&psc=1&tag=&ref=&adgrpid=70569416599&hvpone=&hvptwo=&hvadid=354074254997&hvpos=&hvnetw=g&hvrand=7371772830080740014&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9055818&hvtargid=pla-777534104593"
         },
         {
-            _id:3,
             name:"30 Outils de sculpture sur argile artisanale",
             image:"/images/03.jpg",
-            category:"equipment stencils",            
+            category:"equipment stencils",       
+            description:"blabla",     
             price:11,
             countInStock:2,
             numReviews:8,
@@ -51,10 +51,10 @@ const data = {
             website:"https://www.wish.com/product/5da56717bfa84d0b75ea680b?hide_login_modal=true&from_ad=goog_shopping&_display_country_code=FR&_force_currency_code=EUR&pid=googleadwords_int&c=%7BcampaignId%7D&ad_cid=5da56717bfa84d0b75ea680b&ad_cc=FR&ad_lang=FR&ad_curr=EUR&ad_price=15.00&campaign_id=8143443324&gclid=EAIaIQobChMIh-zdsPmd8AIVxfhRCh3WBQ4MEAQYBSABEgIDbfD_BwE&share=web"
         },
         {
-            _id:4,
             name:"Kit de sculpture en argile",
             image:"/images/04.jpg",
             category:"equipment stencils",
+            description:"blabla",
             price:9.53,
             countInStock:2,
             numReviews:8,
@@ -62,10 +62,10 @@ const data = {
             website:"https://www.aliexpress.com/item/32953096668.html?albpd=fr32953096668&acnt=248-630-5778&aff_platform=aaf&albpg=539263010115&netw=u&albcp=10191220526&pvid=4bde0ed9-4ccf-45c1-a1ba-0a97ff444e72&sk=UneMJZVf&scm=1007.23534.123999.0&trgt=539263010115&terminal_id=d3f99f2ae5be4c108ca3f9460cc17fa1&needSmbHouyi=false&albbt=Google_7_shopping&src=google&crea=fr32953096668&aff_fcid=c39c584f0cc641c1b8d60295598000b2-1619510467424-04854-UneMJZVf&gclid=EAIaIQobChMIzYeup_qd8AIVRJ3VCh07uQSKEAQYDSABEgKBcPD_BwE&albag=107473525328&aff_fsk=UneMJZVf&albch=shopping&albagn=888888&isSmbAutoCall=false&aff_trace_key=c39c584f0cc641c1b8d60295598000b2-1619510467424-04854-UneMJZVf&rmsg=do_not_replacement&device=c&gclsrc=aw.ds"
         },
         {
-            _id:5,
             name:"Nouvelle roue, disques interchangeables",
             image:"/images/05.jpg",
-            category:"equipment wheel",            
+            category:"equipment wheel",
+            description:"blabla",
             price:59.99,
             countInStock:2,
             numReviews:8,
@@ -73,10 +73,10 @@ const data = {
             website:"https://fra.grandado.com/products/nouvelle-machine-de-roue-de-poterie-de-plateau-tournant-electrique-avec-le-tournevis-hexagonal-de-plateau-adaptateur-de-puissance-bricolage-en-ceramique"
         },
         {
-            _id:6,
             name:"Tour De Poterie professionel",
             image:"/images/06.jpg",
-            category:"equipment wheel",           
+            category:"equipment wheel",
+            description:"blabla",
             price:3.99,
             countInStock:2,
             numReviews:8,
