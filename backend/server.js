8262e65605331d047aa9799b802177bdd7496a0e
BOCKELADNT Amelie <ameliebock@hotmail.fr>
2021-05-05 10:42:08 +0200

    Connection to backend database of products

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
+import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
@@ -11,24 +11,8 @@ mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost/potterspot', {
     useCreateIndex: true,
 })
 
-app.get('/api/products/:id', (req, res) => {
-    
-    const productId = parseInt(req.params.id);
-    const product = data.products.find((x) => x._id === productId);
-    
-    if (product) {
-        res.send(product);
-    } else {
-        res.status(404).send({ message: 'Product Not Found' });
-    }
-});
-
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-});
-
 app.use('/api/users', userRouter);
-
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
@@ -37,8 +21,8 @@ app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
 });
 
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Up and running on port ${PORT}`);
 });
