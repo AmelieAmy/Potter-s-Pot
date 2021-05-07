eb5de8f92b92037313efbcb828ee1611c6ce708a
BOCKELADNT Amelie <ameliebock@hotmail.fr>
2021-05-07 18:22:51 +0200

    Add Paypal button and paypal fonctionalities

--- frontend/src/components/screenComponents/OrderScreen.js
+++ frontend/src/components/screenComponents/OrderScreen.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import Axios from 'axios';
+import { PayPalButton } from 'react-paypal-button-v2';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { detailsOrder } from '../../redux_files/actions/orderActions';
@@ -10,14 +12,42 @@ const OrderScreen = (props) => {
 
     const orderId = props.match.params.id;
 
+    const [sdkReady, setSdkReady] = useState(false);
+
     const orderDetails = useSelector((state) => state.orderDetails);
     const { order, loading, error } = orderDetails;
 
     const dispatch = useDispatch();
 
     useEffect(() => {
-      dispatch(detailsOrder(orderId));
-    }, [dispatch, orderId]);
+        const addPayPalScript = async () => {
+            const { data } = await Axios.get('/api/config/paypal');
+            const script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = `https://www.paypal.com/sdk/js?client-id=${data}`;
+            script.async = true;
+            script.onload = () => {
+                setSdkReady(true);
+            };
+            document.body.appendChild(script);
+        };
+        if (!order) {
+            dispatch(detailsOrder(orderId));
+        } else {
+            if (!order.isPaid) {
+                if (!window.paypal) {
+                    addPayPalScript();
+                } else {
+                    setSdkReady(true);
+                }
+            }
+        }
+    }, [dispatch, order, orderId, sdkReady]);
+      
+        const successPaymentHnadler = () => {
+          // TODO: dispatch pay order
+        };
+      
 
 
     return (
@@ -129,6 +159,20 @@ const OrderScreen = (props) => {
                                         </div>
                                     </div>
                                 </li>
+
+                                <li>
+                                    <PayPalButton amount={order.totalPrice} onSuccess={successPaymentHnadler} ></PayPalButton>
+                                </li>
+                                {/* {!order.isPaid && (
+                                    <li>
+                                        {!sdkReady ? (
+                                            <LoadingBox></LoadingBox>
+                                        ) : (
+                                            <PayPalButton amount={order.totalPrice} onSuccess={successPaymentHnadler} ></PayPalButton>
+                                        )}
+                                    </li>
+                                )} */}
+
                             </ul>
                         </div>
                     </div>
